5e87652fd8423f1fff3ab12067861658f26ac408
Julia Leithold <juliaavancisoares@gmail.com>
2024-09-19 19:37:04 -0300

    correção

--- init.lua
+++ init.lua
@@ -5,4 +5,17 @@ return {
     opts = { ensure_installed = { "html-lsp" } },
   }
 
- 
+  return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  }
