b2c86343a5bfadfc8d6f75fe73ecc7da7a6ca5d1
Julia Leithold <juliaavancisoares@gmail.com>
2024-09-19 19:35:27 -0300

    atualizações lua

--- init.lua
+++ init.lua
@@ -1,1 +1,21 @@
 require("config.lazy")
+
+return {
+    "mason.nvim",
+    opts = { ensure_installed = { "html-lsp" } },
+  }
+
+  return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  }
