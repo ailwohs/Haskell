8e71fb48d207a641321d647e7c1d0707016fc6bf
Julia Leithold <juliaavancisoares@gmail.com>
2024-10-01 21:37:52 -0300

    adicionando arduino e nada funciona

--- init.lua
+++ init.lua
@@ -4,18 +4,3 @@ return {
     "mason.nvim",
     opts = { ensure_installed = { "html-lsp" } },
   }
-
-  return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "right",
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-      },
-    },
-  }
