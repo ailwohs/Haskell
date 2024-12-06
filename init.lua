f95f4133e36c9ef906a6e096776f1ea86e011ae4
Julha <juliaavancisoares@gmail.com>
2024-12-06 19:10:46 -0300

    alterações

--- init.lua
+++ init.lua
@@ -3,19 +3,5 @@ require("config.lazy")
 return {
     "mason.nvim",
     opts = { ensure_installed = { "html-lsp" } },
-  }
 
-  return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "right",
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-      },
-    },
   }
