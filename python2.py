116247f89182edfa2afea69df6134de41cc9597d
Julha <juliaavancisoares@gmail.com>
2025-01-30 20:42:09 -0300

    alterações

--- python2.py
+++ python2.py
@@ -6,3 +6,17 @@ print("Seu nome é:", nome, "sua idade é:", idade, "seu peso é:", peso)
 meses=idade*12
 print("Você já viveu", meses, "meses")
 
+ano=idade*365
+print("E", ano, "tudo isso de anos")
+
+'''#Programa que pede dois numeros ao usuario e apresenta o contexto todo em print, adição, 
+multiplicação etc...'''
+
+
+n1=float(input("Digite o primeiro número \n"))
+n2=float(input("Digite o segundo número \n"))
+
+soma = n1+n2
+mut = n1**n2
+divi= n1/n2
+print("A soma dos números é:", soma, "E a multiplicação é:", mut , "a divisão é:", divi)                 
